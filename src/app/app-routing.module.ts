49cb179b5322ef7685ee4a4a5489d1e5dc6e4f70
Lizamari Diesel <lizamari.diesel@absa.co.za>
2019-07-27 17:24:18 +0200

    generated inventory component and added to navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrderComponent } from './order/order.component';
 import { HomeComponent } from './home/home.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'order', component: OrderComponent }
+  { path: 'order', component: OrderComponent },
+  { path: 'inventory', component: InventoryComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { OrderComponent } from './order/order.component';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OrderComponent,
     HomeComponent,
-    NavComponent
+    NavComponent,
+    InventoryComponent
   ],
   imports: [
     BrowserModule,
